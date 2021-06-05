d01884419e9cbe567805c238df02a44c22f9f7a2
K0R0VA <yarykov.m@edu.narfu.ru>
2021-06-05 21:25:39 +0300

    Настроил иконки

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ Vue.config.productionTip = false
 
 
 import '@/assets/styles/main.scss';
+import '@mdi/font/css/materialdesignicons.css'
 
 new Vue({
   router,
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,12 +1,24 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import ru from 'vuetify/src/locale/ru.ts';
+//import {preset} from 'vuetify/src/presets/default/index.ts';
 
 Vue.use(Vuetify);
 
-export default new Vuetify({
+const options = {
     theme: {
         theme: {
             options: { customProperties: true },
         },
+    },
+    lang: {
+        current: ru
+    },
+    icons: {
+        iconfont: 'mdi'
     }
+}
+
+export default new Vuetify({
+    ...options
 });
