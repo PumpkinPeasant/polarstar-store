29b25a85dac962f88633e8c4390cb338fe6659c7
K0R0VA <yarykov.m@edu.narfu.ru>
2021-06-05 20:29:58 +0300

    Добавил компонент LoginDialog.vue

---
+++ src/components/dialogs/LoginDialog.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-dialog>
+    <template v-slot:activator="{on,}">
+      <slot v-on="on"></slot>
+    </template>
+    <v-card>
+      <v-card-title></v-card-title>
+      <v-card-text></v-card-text>
+      <v-card-actions></v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: "LoginDialog"
+}
+</script>
+
+<style scoped>
+
+</style>
